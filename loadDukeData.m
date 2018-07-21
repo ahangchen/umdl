f323dabc1795a47698c72842c46b615d4885fb1d
ahangchen <1418659400@qq.com>
2018-07-22 00:20:57 +0800

    add load duke

---
+++ loadDukeData.m
@@ -0,0 +1,47 @@
+%ntrials=4;
+
+%%  ��ȡ����   %%
+load('data/Feature/duke_norm_feature.mat'); %��ȡhand-draft����
+% h_feature=feature;
+%   load('data/cnndata/VIPeR_CNNfeature.mat'); %��ȡCNN����
+%  cnn_feature=feature;
+% feature=[cnn_feature/20;h_feature];
+clear pattribute;
+%% ��ݼ����� %%
+ load('data/Split/duke_split_kcca.mat');  %KCCA �������õ��Ļ��ַ�ʽ
+
+labelsAtrain=trials.labelsAtrain;
+labelsAtest=trials.labelsAtest;
+pattribute=[];
+featureTrain=[];%zeros(size(feature,1),length(labelsAtrain)*2);
+labelsTrain=[];%zeros(1,length(labelsAtrain)*2);
+   camTrain=[];%zeros(1,length(labelsAtrain)*2);
+   featureTest=[];%zeros(size(feature,1),length(labelsAtest)*2);
+   labelsTest=[];%zeros(1,length(labelsAtest)*2);
+   camTest=[];%zeros(1,length(labelsAtest)*2);
+   indx=zeros(174,1);
+   for i=1:174%add for testing,every29-1
+       indx(i)=29*i;%%%
+   end%%% add 29-1
+   for i=1:length(labelsAtrain)
+       index=find(ID(:)==labelsAtrain(i)-1);
+       featureTrain=[featureTrain,feature(:,index)];
+
+       labelsTrain=[labelsTrain,ID(index)];
+       camTrain=[camTrain,camID(:,index)];
+   end
+     featureTrain(indx,:)=[];
+     for i=1:length(labelsAtest)
+       index=find(ID(:)==labelsAtest(i)-1);
+       featureTest=[featureTest,feature(:,index)];
+
+       labelsTest=[labelsTest,ID(index)];
+       camTest=[camTest,camID(:,index)];
+     end
+     featureTest(indx,:)=[];
+     F_MT_train{target_task}=featureTrain;
+%F_MT_train{target_task}=featureTrain(175:5138,:);%change for testing last 4964
+weight(target_task)=1;
+ clear trials;
+
+  disp('Target data loaded');
--- loadSourceData.m
+++ loadSourceData.m
@@ -1,13 +1,13 @@
 
 %datanames={'PRID','CAVIAR4REID','i-LID','CUHK'};
 datanames={'VIPeR','VIPeR','VIPeR','VIPeR'};
-tasks=[1,2,3]; 
+tasks=[1,2,3];
 weight=[0.5,0.5,0.5];
 t=0;
 for d=tasks
     t=t+1;
    dataname=datanames{d};
-   load(['/home/zzy/下载/data/Feature/',dataname,'_norm_feature.mat']);
+   load(['data/Feature/',dataname,'_norm_feature.mat']);
    F_MT_train{t}=feature(1:4964,:);
    M{t}=constructWmatrix(ID,[],[],1);
 end
